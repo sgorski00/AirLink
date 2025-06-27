b3f5b3f03f68077cc682b4ab1c15cfb82377123a
Sebastian Górski <sebastiangorski00@gmail.com>
2025-06-27 10:28:12 +0200

    feat(routing): added basic route to the swager docs

--- src/main/java/pl/sgorski/AirLink/controller/SwaggerController.java
+++ src/main/java/pl/sgorski/AirLink/controller/SwaggerController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SwaggerController {
 
-    @GetMapping("/docs")
-    public String redirectToSwaggerUI() {
+    @GetMapping({"/", "/docs", "/swagger"})
+    public String redirectToSwagger() {
         return "redirect:/swagger-ui/index.html";
     }
 }
